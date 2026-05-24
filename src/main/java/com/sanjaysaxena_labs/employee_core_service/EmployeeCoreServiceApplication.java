2e1e9ed08cfe3d38c72507f3bc5f0326f839fd2c
Sanjay Saxena <sanjaysaxena.aiml@gmail.com>
2026-05-24 17:23:10 +0530

    Initialized project

---
+++ src/main/java/com/sanjaysaxena_labs/employee_core_service/EmployeeCoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sanjaysaxena_labs.employee_core_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCoreServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanjaysaxena_labs/employee_core_service/EmployeeCoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjaysaxena_labs.employee_core_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
